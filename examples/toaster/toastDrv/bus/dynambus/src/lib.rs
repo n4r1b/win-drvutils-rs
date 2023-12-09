a48a6e8890f2788a04aed7a51ee430a84f5507d4
n4r1b <jredondo43@gmail.com>
2023-12-09 01:54:55 +0100

    Add initial structure for toaster sample using windows-drivers-rs

---
+++ examples/toaster/toastDrv/bus/dynambus/src/lib.rs
@@ -0,0 +1,25 @@
+#![no_std]
+
+#[cfg(not(test))]
+extern crate wdk_panic;
+
+#[cfg(not(test))]
+use wdk_alloc::WDKAllocator;
+
+#[cfg(not(test))]
+#[global_allocator]
+static GLOBAL_ALLOCATOR: WDKAllocator = WDKAllocator;
+
+use wdk_sys::{
+    DRIVER_OBJECT,
+    NTSTATUS,
+    PCUNICODE_STRING,
+};
+
+#[export_name = "DriverEntry"] // WDF expects a symbol with the name DriverEntry
+pub unsafe extern "system" fn driver_entry(
+    driver: &mut DRIVER_OBJECT,
+    registry_path: PCUNICODE_STRING,
+) -> NTSTATUS {
+    0
+}
